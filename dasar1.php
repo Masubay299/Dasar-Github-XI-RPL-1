2bfff44830caece6d6f4a2ae90509ae3952ceea4
Masubay299 <bayu@smkassalaambandung.sch.id>
2022-01-19 11:02:08 +0700

    First Commit Github

---
+++ dasar1.php
@@ -0,0 +1,9 @@
+<?php
+
+const pi = 3.14;
+$jari = 12;
+echo "Cara Menghitung Luas Lingkaran <br>";
+$luas = pi * $jari * $jari;
+echo "Jari-jari = 12 <br>";
+echo "Rumusnya = pi * jari2 * jari2 <br>";
+echo "Luas lingkarannya adalah $luas";
---
+++ dasar2.php
@@ -0,0 +1,5 @@
+<?php
+// menggunakan variabel
+$saya = "Pacar Orang";
+echo "I love you $saya";
+?>
